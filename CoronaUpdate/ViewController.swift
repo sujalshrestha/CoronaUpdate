4bb3ee671c59c7cbf0ee5a431d2fda3d70dc7308
Sujal Shrestha <suj9763@gmail.com>
2020-03-27 09:07:58 +0545

    Initial Commit

---
+++ CoronaUpdate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaUpdate
+//
+//  Created by Sujal on 3/27/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
